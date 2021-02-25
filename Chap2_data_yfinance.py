66baa30c4d414447ab953499a84f5f01429ca8bd
ljshope <70751522+ljshope@users.noreply.github.com>
2021-02-25 16:01:51 +0000

    Add files via upload

---
+++ Chap2_data_yfinance.py
@@ -0,0 +1,28 @@
+
+import pandas as pd
+import yfinance as yf
+
+symbol ='FB'
+ticker = yf.Ticker(symbol)
+
+pd.Series(ticker.info).head(20)
+
+data = ticker.history(period='5d',
+                      interval='1m',
+                      start=None,
+                      end=None,
+                      actions=True,
+                      auto_adjust=True,
+                      back_adjust=False)
+#data.info()
+
+#ESG
+ticker.sustainability
+
+#analyst
+ticker.recommendations.info()
+ticker.recommendations.tail(10)
+
+ticker.options
+expiration = ticker.options[0]
+options = ticker.option_chain(expiration)
